0982f7216d3937eb94871b00b755c4928f0816f5
farazzshaikh <farazzshaikh@gmail.com>
2026-06-28 19:29:16 +0400

    Add demo UI overlay with title, description, author, and View all demos link

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import { UI } from "./UI";
 import "./index.css";
 
 const container = document.getElementById("root");
@@ -7,5 +8,6 @@ const root = createRoot(container);
 root.render(
   <>
     <App />
+    <UI />
   </>
 );
